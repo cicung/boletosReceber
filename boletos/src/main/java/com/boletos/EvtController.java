fc532b6b99fc57311ee15e884300e42b19bd2b51
cicung <cicung2013@gmail.com>
2020-03-14 18:14:38 -0300

    Create EvtController.java

---
+++ boletos/src/main/java/com/boletos/EvtController.java
@@ -0,0 +1,16 @@
+package com.boletos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class EvtController {
+
+	
+	@RequestMapping("/")
+	 public String index() {
+		return "index";
+	}
+}
