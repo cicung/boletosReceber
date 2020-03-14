af865e74294e058de03ffb2a7c4b305b5342cb13
cicung <cicung2013@gmail.com>
2020-03-14 18:08:28 -0300

    Create App.java

---
+++ contasReceber/src/main/java/com/boletos/App.java
@@ -0,0 +1,13 @@
+package com.boletos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
