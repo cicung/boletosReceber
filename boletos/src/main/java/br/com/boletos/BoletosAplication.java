f461013d603996db04c7d5979a172174d0c7e88e
cicung <cicung2013@gmail.com>
2020-03-15 15:42:30 -0300

    Create BoletosAplication.java

---
+++ boletos/src/main/java/br/com/boletos/BoletosAplication.java
@@ -0,0 +1,15 @@
+package br.com.boletos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoletosAplication {
+	
+	public static void main (String[] args) {
+		SpringApplication.run(BoletosAplication.class, args);
+		
+		
+	}
+
+}
