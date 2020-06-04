0d85e2685e7ebe5a9772a65f76c8690def49a78e
cicung <cicung2013@gmail.com>
2020-06-04 20:16:38 -0300

    inclusão do update e delete

--- src/main/java/br/com/boletos/controller/BoletoController.java
+++ src/main/java/br/com/boletos/controller/BoletoController.java
@@ -1,11 +1,8 @@
 package br.com.boletos.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,41 +36,23 @@ public class BoletoController {
 		return new RedirectView("/boleto/list");
 	}
 	
-	
-	//TODO
-	/**
-	 * Criar um novo método com a rota /edit
-	 * Esse método deve receber o id do boleto
-	 * 
-	 * Com esse ID, buscar o boleto no banco de dados e retornar pra tela
-	 */
-
 	@GetMapping("/edit/{codigoBoleto}")
 	public String edit(@PathVariable("codigoBoleto")Integer codigo, Model model) {	
 		 Boleto boleto = rep.listById(codigo);		 
 		 model.addAttribute("boleto", boleto);
-		return "/boleto/edit";
+		 return "/boleto/edit";
 		
 	}
 	
-	//TODO 2
-	/**
-	 * Começar esse, somente quando o passo acima estiver tudo ok
-	 * 
-	 * Criar um novo método com a rota /update
-	 * Esse método vai receber um único objeto (Boleto)
-	 * Salvar esse objeto no banco
-	 */
-	@PostMapping("/update/{codigoBoleto}")
-	public String update(@PathVariable("codigoBoleto")Integer codigo, @Valid Boleto boleto, BindingResult result, Model model) {			 
-		    if (result.hasErrors()) {
-		        boleto.setCodigoBoleto(codigo);
-		        return "/boleto/update";
-		    }
-		
-		    rep.insert(boleto);		    
-		    model.addAttribute("boleto", rep.list());
-		return "/boleto/list";
-		
+	@PostMapping("/update")
+	public RedirectView update(Boleto boleto) {			     
+		  rep.edit(boleto);
+		  return new RedirectView ("/boleto/list");
+	}
+	
+	@GetMapping("/delete/{codigoBoleto}")
+	public RedirectView deleteById(@PathVariable("codigoBoleto")Integer codigo) {
+		  rep.deleteById(codigo);
+		  return new RedirectView ("/boleto/list");
 	}
 }
